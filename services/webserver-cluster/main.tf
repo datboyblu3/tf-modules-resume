debddfa83313d1355d848d651371ca0bfa036a92
alexrf45 <fonalex45@gmail.com>
2023-02-04 01:56:14 -0500

    lint

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -2,9 +2,9 @@
 
 data "terraform_remote_state" "db" {
   backend = "s3"
-  config = {
+  config  = {
     bucket = var.db_remote_state_bucket
-    key = var.db_remote_state_key
+    key    = var.db_remote_state_key
     region = "us-east-2"
   }
 }
@@ -19,16 +19,14 @@ locals {
   all_ips      = ["0.0.0.0/0"]
 }
 
-
-
 resource "aws_launch_configuration" "example" {
-  image_id = "ami-0fb653ca2d3203ac1"
-  instance_type = var.instance_type
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = var.instance_type
   security_groups = [aws_security_group.instance.id]
-  user_data = templatefile("${path.module}/user-data.sh", {
-    server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
+  user_data       = templatefile("${path.module}/user-data.sh", {
+    server_port   = var.server_port
+    db_address    = data.terraform_remote_state.db.outputs.address
+    db_port       = data.terraform_remote_state.db.outputs.port
   })
 	lifecycle {
 		create_before_destroy = true
@@ -40,8 +38,8 @@ resource "aws_autoscaling_group" "example" {
 	vpc_zone_identifier  = data.aws_subnets.default.ids
 	target_group_arns    = [aws_lb_target_group.asg.arn]
 	health_check_type    = "ELB"
-	min_size = var.min_size
-	max_size = var.max_size
+	min_size             = var.min_size
+	max_size             = var.max_size
 	tag {
 		key                 = "Name"
 		value               = var.cluster_name
@@ -55,9 +53,9 @@ resource "aws_security_group" "instance" {
 	name = "${var.cluster_name}-instance"
 	
 	ingress {
-		from_port = var.server_port
-		to_port = var.server_port
-		protocol = local.tcp_protocol 
+		from_port   = var.server_port
+		to_port     = var.server_port
+		protocol    = local.tcp_protocol 
 		cidr_blocks = local.all_ips
 	}
 }
@@ -70,15 +68,15 @@ resource "aws_security_group_rule" "allow_http_inbound" {
     type              = "ingress"
     security_group_id = aws_security_group.alb.id
 
-	  from_port		= local.http_port
+	  from_port			= local.http_port
 		to_port			= local.http_port
 		protocol		= local.tcp_protocol
-		cidr_blocks = local.all_ips
+		cidr_blocks 	= local.all_ips
 }
 
 resource "aws_security_group_rule" "allow_all_outbound" { 
 	  type              = "egress"
-    security_group_id = aws_security_group.alb.id
+    security_group_id 	= aws_security_group.alb.id
 
 		from_port   = local.any_port
 		to_port     = local.any_port
@@ -96,7 +94,7 @@ data "aws_vpc" "default" {
 data "aws_subnets" "default" {
 	filter {
 		name 	 = "vpc-id"
-		values = [data.aws_vpc.default.id]
+		values 	 = [data.aws_vpc.default.id]
 	}
 }
 
@@ -125,25 +123,25 @@ resource "aws_lb_listener" "http" {
 }
 
 resource "aws_lb_target_group" "asg" {
-	name = "${var.cluster_name}-tgt-grp"
-	port = var.server_port
+	name 	 = "${var.cluster_name}-tgt-grp"
+	port 	 = var.server_port
 	protocol = "HTTP"
-	vpc_id = data.aws_vpc.default.id
+	vpc_id   = data.aws_vpc.default.id
 	
 	health_check {
-		path = "/"
-		protocol = "HTTP"
-		matcher = "200"
-		interval = 15
-		timeout = 3
-		healthy_threshold = 2
+		path                = "/"
+		protocol            = "HTTP"
+		matcher             = "200"
+		interval            = 15
+		timeout             = 3
+		healthy_threshold   = 2
 		unhealthy_threshold = 2
 	}
 }
 
 resource "aws_lb_listener_rule" "asg" {
 	listener_arn = aws_lb_listener.http.arn
-	priority = 100
+	priority 	 = 100
 	condition {
 		path_pattern {
 		values = ["*"]
