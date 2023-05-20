69a8654260ced802c3573c4e9a38ce517c5c5d6d
alexrf45 <fonalex45@gmail.com>
2023-05-20 01:41:02 -0400

    testing

--- services/static-site-cloudflare/providers.tf
+++ services/static-site-cloudflare/providers.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.0.0"
+      version = "~> 4.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "2.19.2"
+      version = "~> 3.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.1.0"
+      version = "~> 3.0"
     }
   }
 }
