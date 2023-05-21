e961456b5960216611af08f4483671cde6b21d86
alexrf45 <fonalex45@gmail.com>
2023-05-21 15:03:23 -0400

    fu

--- services/static-site-cloudflare/cloudflare.tf
+++ services/static-site-cloudflare/cloudflare.tf
@@ -4,7 +4,7 @@ data "cloudflare_zone" "domain" {
 
 resource "cloudflare_record" "root" {
   for_each = {
-    for dvo in aaws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
       name  = dvo.resourec_record_name
       value = dvo.resource_record_value
       type  = dvo.resource_record_type
