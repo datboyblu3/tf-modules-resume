b7db68efb33c2158d416e57790f2d17fadc08bd0
alexrf45 <fonalex45@gmail.com>
2023-05-21 15:07:13 -0400

    t

--- services/static-site-cloudflare/cdn.tf
+++ services/static-site-cloudflare/cdn.tf
@@ -10,7 +10,7 @@ resource "aws_acm_certificate" "cert" {
 
 resource "aws_acm_certificate_validation" "validate" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = ["${cloudflare_record.root.hostname}"]
+  validation_record_fqdns = [cloudflare_record.root[each.key]]
 }
 
 resource "aws_cloudfront_origin_access_identity" "site" {
