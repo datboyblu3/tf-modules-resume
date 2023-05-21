81eb8213a527adb1d67d18a129f647ae7ab6fe7e
alexrf45 <fonalex45@gmail.com>
2023-05-21 13:43:31 -0400

    testing

--- services/static-site-cloudflare/cloudflare.tf
+++ services/static-site-cloudflare/cloudflare.tf
@@ -2,17 +2,6 @@ data "cloudflare_zone" "domain" {
   name = var.site_domain
 }
 
-resource "cloudflare_record" "cname" {
-  # depends_on = [aws_cloudfront_distribution.dist]
-  zone_id = data.cloudflare_zone.domain.id
-  name    = var.name
-  value   = var.site_domain
-  type    = "CNAME"
-
-  ttl     = 1
-  proxied = false
-}
-
 resource "cloudflare_record" "root" {
   zone_id = data.cloudflare_zone.domain.id
   name    = "@"
