c83ea29ffa3580b0c77354804816a295ec7178d0
alexrf45 <fonalex45@gmail.com>
2023-05-21 15:09:33 -0400

    yes

--- services/static-site-cloudflare/cloudflare.tf
+++ services/static-site-cloudflare/cloudflare.tf
@@ -3,17 +3,9 @@ data "cloudflare_zone" "domain" {
 }
 
 resource "cloudflare_record" "root" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name  = dvo.resourec_record_name
-      value = dvo.resource_record_value
-      type  = dvo.resource_record_type
-
-    }
-  }
-  name    = each.value.name
-  type    = each.value.type
-  value   = each.value.value
+  name    = var.name
+  type    = "CNAME"
+  value   = "www.${var.site_domain}"
   zone_id = data.cloudflare_zone.domain.id
   ttl     = 1
 }
