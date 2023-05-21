141dec8a37d20ee235f4ec14606e0dc698d19722
alexrf45 <fonalex45@gmail.com>
2023-05-21 16:21:37 -0400

    hmm

--- services/static-site-cloudflare/cloudflare.tf
+++ services/static-site-cloudflare/cloudflare.tf
@@ -7,7 +7,7 @@ resource "cloudflare_record" "acm" {
 
   zone_id = data.cloudflare_zone.domain.id
   name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
-  value   = [tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value]
+  value   = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value
   type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
 }
 
