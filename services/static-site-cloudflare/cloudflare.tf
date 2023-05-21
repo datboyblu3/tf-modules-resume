fb0d6ba84d8e9f1f4bca624bb71aed22527ef6c0
alexrf45 <fonalex45@gmail.com>
2023-05-21 15:02:27 -0400

    maybe this

--- services/static-site-cloudflare/cloudflare.tf
+++ services/static-site-cloudflare/cloudflare.tf
@@ -3,9 +3,14 @@ data "cloudflare_zone" "domain" {
 }
 
 resource "cloudflare_record" "root" {
+  for_each = {
+    for dvo in aaws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resourec_record_name
+      value = dvo.resource_record_value
+      type  = dvo.resource_record_type
+
+    }
+  }
   zone_id = data.cloudflare_zone.domain.id
-  name    = "@"
-  value   = "www.${aws_acm_certificate.cert.domain_name}"
-  type    = "CNAME"
   ttl     = 1
 }
