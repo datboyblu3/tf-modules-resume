b04acb00791a22ee345b4808178bcc5d9c6481fe
alexrf45 <fonalex45@gmail.com>
2024-01-28 00:37:54 -0500

    testing

--- services/static-site-cloudflare/cloudflare.tf
+++ services/static-site-cloudflare/cloudflare.tf
@@ -12,18 +12,17 @@ resource "cloudflare_record" "acm" {
   name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
   value   = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value
   type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
+  ttl     = 60
+  proxied = false
 }
 
 
 resource "cloudflare_record" "cname" {
   depends_on = [aws_cloudfront_distribution.dist]
-
-
-
-  zone_id = data.cloudflare_zones.domain.id
-  name    = var.site_domain
-  value   = aws_cloudfront_distribution.dist.domain_name
-  type    = "CNAME"
+  zone_id    = data.cloudflare_zones.domain.id
+  name       = var.site_domain
+  value      = aws_cloudfront_distribution.dist.domain_name
+  type       = "CNAME"
 }
 
 
