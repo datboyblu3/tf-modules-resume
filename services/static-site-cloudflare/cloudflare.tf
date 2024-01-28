c4465ef5679d76a3b79f90f4b8d2d7611cb43b65
alexrf45 <fonalex45@gmail.com>
2024-01-27 22:44:33 -0500

    cool

--- services/static-site-cloudflare/cloudflare.tf
+++ services/static-site-cloudflare/cloudflare.tf
@@ -1,11 +1,17 @@
-data "cloudflare_zone" "domain" {
-  name = var.site_domain
+# data "cloudflare_zone" "domain" {
+#   name = var.site_domain
+# }
+
+data "cloudflare_zones" "domains" {
+  filter {
+    name = var.site_domain
+  }
 }
 
 resource "cloudflare_record" "acm" {
   depends_on = [aws_acm_certificate.cert]
 
-  zone_id = data.cloudflare_zone.domain.id
+  zone_id = data.cloudflare_zones.domains.id
   name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
   value   = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value
   type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
@@ -15,7 +21,7 @@ resource "cloudflare_record" "acm" {
 resource "cloudflare_record" "cname" {
   depends_on = [aws_cloudfront_distribution.dist]
 
-  zone_id = data.cloudflare_zone.domain.id
+  zone_id = data.cloudflare_zones.domains.id
   name    = var.site_domain
   value   = aws_cloudfront_distribution.dist.domain_name
   type    = "CNAME"
@@ -26,7 +32,7 @@ resource "cloudflare_record" "cname" {
 resource "cloudflare_record" "www" {
   depends_on = [aws_cloudfront_distribution.dist]
 
-  zone_id = data.cloudflare_zone.domain.id
+  zone_id = data.cloudflare_zones.domains.id
   name    = "www"
   value   = var.site_domain
   type    = "CNAME"
@@ -38,7 +44,7 @@ resource "cloudflare_record" "www" {
 resource "cloudflare_record" "blog" {
   depends_on = [aws_cloudfront_distribution.dist]
 
-  zone_id = data.cloudflare_zone.domain.id
+  zone_id = data.cloudflare_zones.domains.id
   name    = "blog"
   value   = var.site_domain
   type    = "CNAME"
