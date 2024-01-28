28e290087b9654de58813ed95eb7e39385c86392
alexrf45 <fonalex45@gmail.com>
2024-01-27 23:46:22 -0500

    nice

--- services/static-site-cloudflare/cloudflare.tf
+++ services/static-site-cloudflare/cloudflare.tf
@@ -1,11 +1,15 @@
-data "cloudflare_zone" "domain" {
-  name = var.site_domain
+data "cloudflare_zones" "domain" {
+  filter {
+    name = var.site_domain
+  }
+
+
 }
 
 resource "cloudflare_record" "acm" {
   depends_on = [aws_acm_certificate.cert]
 
-  zone_id = data.cloudflare_zone.domain.id
+  zone_id = data.cloudflare_zones.domain.id
   name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
   value   = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value
   type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
@@ -15,7 +19,7 @@ resource "cloudflare_record" "acm" {
 resource "cloudflare_record" "cname" {
   depends_on = [aws_cloudfront_distribution.dist]
 
-  zone_id = data.cloudflare_zone.domain.id
+  zone_id = data.cloudflare_zones.domain.id
   name    = var.site_domain
   value   = aws_cloudfront_distribution.dist.domain_name
   type    = "CNAME"
