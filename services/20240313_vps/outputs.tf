9eb47e6d136acf1bb47d1431b442462ab31c8c7b
alexrf45 <fonalex45@gmail.com>
2024-03-13 18:54:34 -0400

    cool

--- services/20240313_vps/outputs.tf
+++ services/20240313_vps/outputs.tf
@@ -5,7 +5,7 @@ output "id" {
 
 output "vps_public_ip" {
   description = "public ip from Elastic IP allocation"
-  value       = aws_eip.vps_eip.public_ip
+  value       = aws_eip.eip.public_ip
 }
 
 output "private-key" {
