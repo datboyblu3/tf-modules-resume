49f5256425ba18e8a6b203678027cc7aaac845f5
alexrf45 <fonalex45@gmail.com>
2024-03-31 01:00:18 -0400

    clean up

--- services/proxmox-vm-cloudinit/config.tf
+++
@@ -1,9 +0,0 @@
-data "template_file" "userdata" {
-  template = file("${path.module}/templates/userdata.yaml")
-
-  vars = {
-    username       = var.username
-    ssh_public_key = tls_private_key.ssh-ed25519.public_key_openssh
-    packages       = jsonencode(var.packages)
-  }
-}
